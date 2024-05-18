28619c6a29cce3eb8f2e9fd7072f571d87efc1e6
Jia XU <xujia118@hotmail.com>
2024-05-17 22:33:53 -0700

    Modified static

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("./dist"));
+app.use(express.static("../client/test-railway/dist"));
 app.use(express.json())
 app.use(cors())
 
